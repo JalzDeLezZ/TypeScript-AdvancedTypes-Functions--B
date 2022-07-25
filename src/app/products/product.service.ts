f73ea552000f44e448a6dcb208cd0541b220a358
JalzDeLezZ <j4mes.delez@gmail.com>
2022-07-25 02:35:19 -0500

    15-property readonly

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import {Product} from './product.model'
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = products.length + 1; // if the data.id isn't declared as readonly
+  // data.createAt = new Date(2000,2,29); // if the data.createAt isn't declared as readonly
   products.push(data);
 }
